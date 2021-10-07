29d92c5a875cd2c13362a98346bba052e0c7c8ec
YTMartian <1773741250@qq.com>
2021-10-07 20:34:43 +0800

    --

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for port in range(1234, 4321):
-    print('port: ', port) 
-    n, c = input('input n c: ').split(' ')
-    command = 'ab -n {} -c {} -s 100 http://127.0.0.1:{}/login.html'.format(n, c, port)
-    print(command)
-    os.system(command)
